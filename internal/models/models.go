f0088b6a6b5fbdb7aa744aa03746c796d149f44d
IB133 <bii.20@uni-dubna.ru>
2023-02-09 15:14:38 +0300

    uppdate models

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,3 +9,10 @@ type Carriers struct {
 	Id   int    `db:"id" json:"id"`
 	Name string `db:"name" json:"name"`
 }
+
+type User struct {
+	Id       int    `db:"id" json:"id"`
+	Login    string `db:"login" json:"login"`
+	Password string `db:"password" json:"password"`
+	Role     string `db:"role" json:"role"`
+}
